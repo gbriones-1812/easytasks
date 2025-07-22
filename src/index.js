01cd54b0253a50e3da1de8ec9ee2cc32acc6efc4
gbriones-1812 <adriel.bf.fcfm@gmail.com>
2025-07-21 20:38:47 -0600

    script to convert Excel File Data to JSON.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const ExcelToJSON = () => {
+var XLSX = require("xlsx");
 
-}
+const ExcelToJSON = () => {
+    const excel = XLSX.readFile("/Users/gonzalobrions/GitHub/easytasks/Formato_CargaMasiva_2025.xlsx");
+    var sheetName = excel.SheetNames;
+    let data = XLSX.utils.sheet_to_json(excel.Sheets[sheetName[0]]);
+    console.log(data);
+};
 
 ExcelToJSON();
